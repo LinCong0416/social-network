89d037b3e9ffcdf443514913c8fcaf8b36235147
LinCong0416 <49532859+LinCong0416@users.noreply.github.com>
2020-12-01 19:29:56 -0800

    c2 finish

--- node_api/app.js
+++ node_api/app.js
@@ -1,21 +1,37 @@
-const express = require('express')
+const express = require("express")
 const app = express()
+const mongoose = require('mongoose')
 const morgan = require("morgan");
+const dotenv = require('dotenv')
+dotenv.config()
 
+//db
+//liner.416@gmail.com
+//MONGO_URI=//mongodb://localhost/nodeapi
+mongoose
+    .connect(
+        process.env.MONGO_URI,
+        { userNewUrlParser: true}
+    )
+    .then(() => console.log('DB connected'))
+
+mongoose.connection.on("error", err =>{
+    console.log('DB connection error: ${err.message}');
+})
 //bring in routes
-const {getPosts} = require("./routes/post")
+const getPosts = require("./routes/post")
 
 //middleware
 app.use(morgan("dev"));
 
-const myMiddleware = (req, res, next) => {
-    console.log('middleware applied!');
-    next();
-}
+// const myMiddleware = (req, res, next) => {
+//     console.log('middleware applied!');
+//     next();
+// }
 
-app.use(myMiddleware);
+// app.use(myMiddleware);
 
-app.get("/", getPosts);
+app.use("/", getPosts);
 
-const  port = 8080;
+const  port = process.env.PORT || npm8080;
 app.listen(port, () => {console.log('Node JS API is listening on 8080')});
