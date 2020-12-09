daff5abb8842063ab4dfef6b0ca5f5c29eb2c6f4
LinCong0416 <49532859+LinCong0416@users.noreply.github.com>
2020-12-09 09:44:09 -0800

    7.2 finished

--- node_api/app.js
+++ node_api/app.js
@@ -3,6 +3,7 @@ const app = express()
 const mongoose = require('mongoose')
 const morgan = require("morgan")
 const dotenv = require('dotenv')
+const fs = require('fs')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const expressValidator = require('express-validator');
@@ -10,6 +11,20 @@ dotenv.config()
 const postRoutes = require("./routes/post")
 const authRoutes = require("./routes/auth")
 const userRoutes = require("./routes/user")
+const cors = require('cors')
+
+//apiDocs
+app.get('/', (req, res) => {
+    fs.readFile('docs/apiDocs.json', (err, data) => {
+        if (err) {
+            res.status(400).json({
+                error: err
+            })
+        }
+        const docs = JSON.parse(data);
+        res.json(docs);
+    })
+})
 
 //db
 //liner.416@gmail.com
@@ -39,6 +54,7 @@ app.use(expressValidator());
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(cors());
 app.use("/", postRoutes);
 app.use("/", authRoutes);
 app.use("/",userRoutes);
--- node_api/routes/post.js
+++ node_api/routes/post.js
@@ -1,15 +1,27 @@
 const express = require('express')
-const {getPosts, createPost,postByUser} = require('../controllers/post')
+const {
+        getPosts,
+        createPost,
+        postByUser,
+        postById,
+        isPoster,
+        deletePost,
+        updatePost
+    } = require('../controllers/post')
 const {createPostValidator} = require('../validator/index')
 const {requireSignin} = require('../controllers/auth')
 const {userById} = require('../controllers/user')
 
 const router = express.Router()
 
-router.get('/',getPosts);
+router.get('/posts',getPosts);
 router.post('/post/new/:userId', requireSignin, createPost, createPostValidator);
 router.get('/posts/by/:userId', requireSignin,postByUser);
+router.delete('/post/:postId', requireSignin, isPoster, deletePost);
+router.put('/post/:postId', requireSignin, isPoster, updatePost);
+
 // any routes containing userid, our app will first execute userById()
 router.param("userId", userById);
+router.param("postId", postById);
 
 module.exports = router;
