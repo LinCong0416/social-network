fde40c3b40ef400678d2afeec0da317902eaba28
LinCong0416 <49532859+LinCong0416@users.noreply.github.com>
2020-12-18 22:35:11 -0800

    c14

--- node_api/routes/user.js
+++ node_api/routes/user.js
@@ -4,17 +4,25 @@ const {userById,
     getUser,
     updateUser,
     deleteUser,
-    userPhoto
+    userPhoto,
+    addFollowing,
+    addFollower,
+    removeFollowing,
+    removeFollower,
+    findPeople
 } = require('../controllers/user')
 const {requireSignin} = require('../controllers/auth')
 
 const router = express.Router()
 
+router.put("/user/follow", requireSignin, addFollowing, addFollower);
+router.put("/user/unfollow", requireSignin, removeFollowing, removeFollower);
 router.get('/users', allUsers);
 router.get('/user/:userId', requireSignin, getUser);
 router.put('/user/:userId', requireSignin, updateUser);
 router.delete('/user/:userId', requireSignin, deleteUser);
 router.get("/user/photo/:userId",userPhoto)
+router.get("/user/findpeople/:userId", requireSignin, findPeople);
 
 // any routes containing userid, our app will first execute userById()
 router.param("userId", userById);
